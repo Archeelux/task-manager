678ee9e4b7e93e6d70d5ee5e7960063e1ab2105f
Arturs <archeelux@gmail.com>
2019-07-02 22:05:23 +0100

    93. Promise Chaining

---
+++ playground/promise-chaining.js
@@ -0,0 +1,17 @@
+require("../src/db/mongoose");
+const User = require("../src/models/user");
+
+//
+
+User.findByIdAndUpdate("5d128442aa6a7b222ce6b078", { age: 1 })
+    .then(user => {
+        console.log(user);
+
+        return User.countDocuments({ age: 1 });
+    })
+    .then(result => {
+        console.log(result);
+    })
+    .catch(e => {
+        console.log(e);
+    });
