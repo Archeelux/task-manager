2bff1afdd483d96396bde909ecb77da8df47dde0
Arturs <archeelux@gmail.com>
2019-07-03 16:52:33 +0100

    112. Hiding Private Data

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ const TaskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === "GET") {
-//         res.send("GET requests are disabled");
-//     } else next();
-// });
-
-// app.use((req, res, next) => {
-//     res.status(503).send("Website is in maintenance please return later. ty bb");
-// });
-
 app.use(express.json());
 app.use(UserRouter);
 app.use(TaskRouter);
