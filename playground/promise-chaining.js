5b86f0f7e694633955d8e0c69ca430ea21982890
Arturs <archeelux@gmail.com>
2019-07-03 13:14:16 +0100

    96. Async/Await: Part II

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,17 +1,27 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-//
+// //
 
-User.findByIdAndUpdate("5d128442aa6a7b222ce6b078", { age: 1 })
-    .then(user => {
-        console.log(user);
+// User.findByIdAndUpdate("5d128442aa6a7b222ce6b078", { age: 1 })
+//     .then(user => {
+//         console.log(user);
 
-        return User.countDocuments({ age: 1 });
-    })
-    .then(result => {
-        console.log(result);
-    })
-    .catch(e => {
-        console.log(e);
-    });
+//         return User.countDocuments({ age: 1 });
+//     })
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(e => {
+//         console.log(e);
+//     });
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+};
+
+updateAgeAndCount("5d12859778cdc91fbc56a089", 18)
+    .then(count => console.log(count))
+    .catch(e => console.log(e));
