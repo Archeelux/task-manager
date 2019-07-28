12e90c9e88fc3bbebfaa9926e7d72428a55e0eb9
Arturs <archeelux@gmail.com>
2019-07-28 15:23:55 +0100

    126 | Completed

--- src/routers/user.js
+++ src/routers/user.js
@@ -87,8 +87,19 @@ const upload = multer({
     }
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
-    res.send();
-});
+const errorMiddleware = (req, res, next) => {
+    throw new Error("From Middleware");
+};
+
+router.post(
+    "/users/me/avatar",
+    upload.single("avatar"),
+    (req, res) => {
+        res.send();
+    },
+    (error, req, res, next) => {
+        res.status(400).send({ error: error.message });
+    }
+);
 
 module.exports = router;
