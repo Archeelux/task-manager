cdf86f7977f3818509a662caaa13b6b6bc0b3604
Arturs <archeelux@gmail.com>
2019-07-28 15:37:45 +0100

    127 | Completed

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ const TaskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require("multer");
+// const multer = require("multer");
 
-const upload = multer({
-    dest: "images",
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error("Please upload a word document"));
-        }
+// const upload = multer({
+//     dest: "images",
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error("Please upload a word document"));
+//         }
 
-        cb(undefined, true);
-    }
-});
+//         cb(undefined, true);
+//     }
+// });
 
-app.post("/upload", upload.single("upload"), (req, res) => {
-    res.send();
-});
+// app.post("/upload", upload.single("upload"), (req, res) => {
+//     res.send();
+// });
 
 app.use(express.json());
 app.use(UserRouter);
