02fd17142bcbc78a6dce74c59f384828a3db083a
Arturs <archeelux@gmail.com>
2019-07-30 07:49:29 +0100

    142|143|144 COMPLETE

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const UserRouter = require("./routers/user");
-const TaskRouter = require("./routers/task");
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
 
-app.use(express.json());
-app.use(UserRouter);
-app.use(TaskRouter);
-
-// START SERVER
 app.listen(port, () => {
     console.log(`Server is on ${port}`);
 });
